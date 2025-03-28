915064b556351c628b87f5b40a0a5fe75e39f4ec
koki-develop <kou.pg.0131@gmail.com>
2025-03-28 20:10:55 +0900

    export default action

--- actions/ci/hello.ts
+++ actions/ci/hello.ts
@@ -1,7 +1,7 @@
-import * as path from "node:path";
 import { Action } from "../../lib";
 
 const action = new Action();
+
 action.run("echo 'Hello, world!'");
 
-action.build(path.join(__dirname, "hello"));
+export default action;
--- actions/ci/test.ts
+++ actions/ci/test.ts
@@ -1,7 +1,7 @@
-import * as path from "node:path";
 import { Action } from "../../lib";
 
 const action = new Action();
+
 action.run("bun test");
 
-action.build(path.join(__dirname, "test"));
+export default action;
