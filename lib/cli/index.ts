8a005cdc98980efd66aefefebba422b8d1dfff8e
koki-develop <kou.pg.0131@gmail.com>
2025-03-28 20:11:55 +0900

    Implement build command

---
+++ lib/cli/index.ts
@@ -0,0 +1,36 @@
+import * as path from "node:path";
+import { Command } from "commander";
+import { Action } from "../action";
+import { mkdirSync, writeFileSync } from "node:fs";
+
+const program = new Command();
+
+program
+  .command("build")
+  .argument("<path>", "build target path")
+  .action(async (targetPath: string) => {
+    const actionPath = path.resolve(process.cwd(), targetPath);
+    const module = await import(actionPath);
+    const action = module.default;
+
+    if (!(action instanceof Action)) {
+      throw new Error(
+        `Action class is not exported as default from ${actionPath}`
+      );
+    }
+
+    const actionYml = action.toString();
+
+    const actionPathWithoutExtension = path.join(
+      path.dirname(actionPath),
+      path.basename(actionPath, path.extname(actionPath))
+    );
+
+    mkdirSync(actionPathWithoutExtension, { recursive: true });
+    writeFileSync(
+      path.join(actionPathWithoutExtension, "action.yml"),
+      actionYml
+    );
+  });
+
+program.parse(process.argv);
