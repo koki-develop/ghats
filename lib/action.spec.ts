39d7276eb4667e948dd86159c6770a56ed8b5b5f
koki-develop <kou.pg.0131@gmail.com>
2025-03-28 19:46:28 +0900

    Add simple test

---
+++ lib/action.spec.ts
@@ -0,0 +1,18 @@
+import { describe, test, expect } from "bun:test";
+import { Action } from "./action";
+
+describe("Action", () => {
+  test("simple action", () => {
+    const action = new Action();
+    action.run("echo 'Hello, world!'");
+
+    expect(action.toString()).toEqual(
+      `runs:
+  using: composite
+  steps:
+    - run: echo 'Hello, world!'
+      shell: bash
+`
+    );
+  });
+});
