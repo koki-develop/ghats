548578b200b8ce4b8bddf8918679c660dbbca2a4
koki-develop <kou.pg.0131@gmail.com>
2025-03-28 23:58:41 +0900

    Create Workflow class

---
+++ lib/package/workflow.ts
@@ -0,0 +1,29 @@
+import { Job } from "./job";
+
+export type WorkflowConfig = {
+  name: string;
+};
+
+export class Workflow {
+  private readonly _config: WorkflowConfig;
+  private readonly _jobs: Record<string, Job> = {};
+
+  public constructor(name: string, config: Omit<WorkflowConfig, "name">) {
+    this._config = { name, ...config };
+  }
+
+  public job(job: Job): Workflow {
+    this._jobs[job.name] = job;
+    return this;
+  }
+
+  public toJSON(): Record<string, unknown> {
+    return {
+      name: this._config.name,
+      on: { push: {} }, // TODO: from config
+      jobs: Object.fromEntries(
+        Object.entries(this._jobs).map(([name, job]) => [name, job.toJSON()]),
+      ),
+    };
+  }
+}
