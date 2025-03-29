820b1540c1ef12ae7042cf480bddc39c172b1960
koki-develop <kou.pg.0131@gmail.com>
2025-03-29 20:30:42 +0900

    Support arguments

--- lib/cli/index.ts
+++ lib/cli/index.ts
@@ -6,14 +6,22 @@ import { Command } from "commander";
 
 const program = new Command();
 
-program.command("build").action(async () => {
+program.command("build [workflows...]").action(async (args: string[]) => {
   register("@swc-node/register/esm", pathToFileURL("./"));
 
   const githubWorkflowsPath = path.resolve(process.cwd(), ".github/workflows");
-  const workflowPaths = fs
-    .readdirSync(githubWorkflowsPath)
-    .map((file) => path.join(githubWorkflowsPath, file))
-    .filter((file) => file.endsWith(".ts"));
+
+  const workflowPaths: string[] = [];
+  if (args.length > 0) {
+    workflowPaths.push(...args.map((arg) => path.resolve(process.cwd(), arg)));
+  } else {
+    workflowPaths.push(
+      ...fs
+        .readdirSync(githubWorkflowsPath)
+        .map((file) => path.join(githubWorkflowsPath, file))
+        .filter((file) => file.endsWith(".ts")),
+    );
+  }
 
   for (const workflowPath of workflowPaths) {
     const module = await import(workflowPath);
