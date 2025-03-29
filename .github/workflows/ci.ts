22574607649e14000ab6a3c752d701a46a3e241f
koki-develop <kou.pg.0131@gmail.com>
2025-03-29 20:24:46 +0900

    Update ci workflow

--- .github/workflows/ci.ts
+++ .github/workflows/ci.ts
@@ -1,9 +1,15 @@
 import { Job, Workflow } from "../../lib";
 
-const helloJob = new Job("hello", { runsOn: "ubuntu-latest" });
-helloJob.run("echo 'Hello, world!'");
+const workflow = new Workflow("CI");
 
-const workflow = new Workflow("CI", {});
-workflow.job(helloJob);
+workflow.job(
+  new Job("test", { runsOn: "ubuntu-latest" })
+    .uses("actions/checkout@11bd71901bbe5b1630ceea73d27597364c9af683", {
+      with: { "persist-credentials": "false" },
+    })
+    .uses("jdx/mise-action@5083fe46898c414b2475087cc79da59e7da859e8")
+    .run("bun install --frozen-lockfile")
+    .run("bun test"),
+);
 
 export default workflow;
