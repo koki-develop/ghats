fbe57ff782f8e400791b6bfc13f217221b12ca00
koki-develop <kou.pg.0131@gmail.com>
2025-03-29 19:48:54 +0900

    Remove actions/

--- actions/ci/hello.ts
+++
@@ -1,7 +0,0 @@
-import { Action } from "../../lib";
-
-const action = new Action();
-
-action.run("echo 'Hello, world!'");
-
-export default action;
--- actions/ci/test.ts
+++
@@ -1,7 +0,0 @@
-import { Action } from "../../lib";
-
-const action = new Action();
-
-action.run("bun test");
-
-export default action;
