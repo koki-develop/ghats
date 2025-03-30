3172e93ad93fd8d1ced8b5a64fd8dcfe6e32d837
koki-develop <kou.pg.0131@gmail.com>
2025-03-30 19:40:07 +0900

    fix: Update description

--- lib/cli/index.ts
+++ lib/cli/index.ts
@@ -4,7 +4,13 @@ import { install } from "./install";
 
 const program = new Command();
 
-program.command("install [actions...]").action(install);
-program.command("build [workflows...]").action(build);
+program
+  .command("install [actions...]")
+  .description("install actions to use in workflows")
+  .action(install);
+program
+  .command("build [workflows...]")
+  .description("build github actions workflows")
+  .action(build);
 
 program.parse(process.argv);
