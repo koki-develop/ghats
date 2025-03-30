0480d433a249933f4b2e3d66859944e137e5621a
koki-develop <kou.pg.0131@gmail.com>
2025-03-30 22:30:28 +0900

    chore: Enable bun cache

--- .github/workflows/_helpers.ts
+++ .github/workflows/_helpers.ts
@@ -15,7 +15,18 @@ export function setupJob(id: string, config: SetupJobConfig): Job {
   }
 
   if (config.withBun) {
-    job.uses(action("jdx/mise-action")).run("bun install --frozen-lockfile");
+    job
+      .uses(action("jdx/mise-action"))
+      .uses(
+        action("actions/cache", {
+          with: {
+            path: "~/.bun/install/cache",
+            key: "${{ runner.os }}-bun-${{ hashFiles('**/bun.lock') }}",
+            "restore-keys": "${{ runner.os }}-bun-",
+          },
+        }),
+      )
+      .run("bun install --frozen-lockfile");
   }
 
   return job;
