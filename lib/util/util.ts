491b941d7c2b10164593c118d0687465d2cb0a7f
koki-develop <kou.pg.0131@gmail.com>
2025-03-31 03:24:13 +0900

    chore: Implement removeUndefinedKeys function

--- lib/util/util.ts
+++ lib/util/util.ts
@@ -4,3 +4,11 @@ export function toUpperCamelCase(str: string): string {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join("");
 }
+
+export function removeUndefinedKeys<T extends Record<string, unknown>>(
+  obj: T,
+): Partial<T> {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([_, value]) => value !== undefined),
+  ) as Partial<T>;
+}
