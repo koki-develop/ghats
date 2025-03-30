7c64384eef48dce388dcb5212d270d5e4b9df1f6
koki-develop <kou.pg.0131@gmail.com>
2025-03-30 10:24:21 +0900

    fix: Export on

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export * from ".ghats/action";
 export * from "./package/job";
+export * from "./package/on";
 export * from "./package/permission";
 export * from "./package/step";
 export * from "./package/workflow";
