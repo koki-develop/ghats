ba6df478b937115d0cda31e06b121bba860c5469
koki-develop <kou.pg.0131@gmail.com>
2025-03-31 19:42:23 +0900

    fix(job): Add tests for job

--- lib/package/job.spec.ts
+++ lib/package/job.spec.ts
@@ -2,12 +2,78 @@ import { describe, expect, test } from "bun:test";
 import { Job } from "./job";
 
 describe("Job", () => {
-  test("simple job", () => {
-    const job = new Job("test", { runsOn: "ubuntu-latest" });
-    job.run("echo 'Hello, world!'");
-    expect(job.toJSON()).toEqual({
-      "runs-on": "ubuntu-latest",
-      steps: [{ run: "echo 'Hello, world!'" }],
-    });
+  test.each<[Job, Record<string, unknown>]>([
+    [
+      new Job("test", { runsOn: "ubuntu-latest" }).run("echo 'Hello, world!'"),
+      {
+        "runs-on": "ubuntu-latest",
+        steps: [{ run: "echo 'Hello, world!'" }],
+      },
+    ],
+    [
+      new Job("test", {
+        runsOn: "ubuntu-latest",
+        permissions: {
+          contents: "read",
+          pullRequests: "write",
+        },
+      }),
+      {
+        "runs-on": "ubuntu-latest",
+        permissions: {
+          contents: "read",
+          "pull-requests": "write",
+        },
+        steps: [],
+      },
+    ],
+    [
+      new Job("test", {
+        runsOn: "ubuntu-latest",
+        timeoutMinutes: 10,
+      }),
+      {
+        "runs-on": "ubuntu-latest",
+        "timeout-minutes": 10,
+        steps: [],
+      },
+    ],
+    [
+      new Job("test", {
+        runsOn: "ubuntu-latest",
+        outputs: {
+          "output-name": "output-value",
+        },
+      }).run("echo 'Hello, world!'"),
+      {
+        "runs-on": "ubuntu-latest",
+        outputs: { "output-name": "output-value" },
+        steps: [{ run: "echo 'Hello, world!'" }],
+      },
+    ],
+    [
+      new Job("test", {
+        runsOn: "ubuntu-latest",
+        needs: "job-name",
+      }).run("echo 'Hello, world!'"),
+      {
+        "runs-on": "ubuntu-latest",
+        needs: "job-name",
+        steps: [{ run: "echo 'Hello, world!'" }],
+      },
+    ],
+    [
+      new Job("test", {
+        runsOn: "ubuntu-latest",
+        if: "always()",
+      }).run("echo 'Hello, world!'"),
+      {
+        "runs-on": "ubuntu-latest",
+        if: "always()",
+        steps: [{ run: "echo 'Hello, world!'" }],
+      },
+    ],
+  ])("job.toJSON(%j) -> %j", (job, expected) => {
+    expect(job.toJSON()).toEqual(expected);
   });
 });
