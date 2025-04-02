ef6741362fb5a50e2b64c49de2a2c09f6fbc5b8a
Koki Sato <kou.pg.0131@gmail.com>
2025-04-02 23:18:58 +0900

    feat: Implement `build` UI (#55)
    * chore: Install ink
    
    * chore: build/index.ts -> build/index.tsx
    
    * chore: Install react
    
    * chore: Create Spinner component
    
    * feat: Implement build ui
    
    * fix: Fix ui

---
+++ lib/cli/build/ui.tsx
@@ -0,0 +1,63 @@
+import { Box, Text } from "ink";
+import Spinner from "../ui/Spinner";
+import { getBuildTargetPath } from ".";
+
+type BuildUIProps = {
+  workflowPaths: string[];
+  currentIndex: number;
+};
+
+type Status = "pending" | "building" | "done";
+
+function getStatus(index: number, currentIndex: number): Status {
+  if (index < currentIndex) return "done";
+  if (index === currentIndex) return "building";
+  return "pending";
+}
+
+export default function BuildUI({ workflowPaths, currentIndex }: BuildUIProps) {
+  return (
+    <Box flexDirection="column">
+      {workflowPaths.map((workflowPath, index) => (
+        <WorkflowPath
+          key={workflowPath}
+          status={getStatus(index, currentIndex)}
+          workflowPath={workflowPath}
+        />
+      ))}
+    </Box>
+  );
+}
+
+type WorkflowPathProps = {
+  status: Status;
+  workflowPath: string;
+};
+
+function WorkflowPath({ status, workflowPath }: WorkflowPathProps) {
+  switch (status) {
+    case "pending":
+      return (
+        <Text dimColor>
+          {"  "}
+          {workflowPath}
+        </Text>
+      );
+    case "building":
+      return (
+        <Text>
+          <Spinner />
+          <Text dimColor> {workflowPath}</Text>
+        </Text>
+      );
+    case "done":
+      return (
+        <Text>
+          <Text color="green" bold>
+            ✔
+          </Text>
+          <Text bold> {getBuildTargetPath(workflowPath)}</Text>
+        </Text>
+      );
+  }
+}
