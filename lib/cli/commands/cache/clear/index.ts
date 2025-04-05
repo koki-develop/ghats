a696652191d422bec884d5688a2212d83668d3e7
koki-develop <kou.pg.0131@gmail.com>
2025-04-05 15:24:32 +0900

    fix: Enable force

--- lib/cli/commands/cache/clear/index.ts
+++ lib/cli/commands/cache/clear/index.ts
@@ -4,6 +4,6 @@ import * as path from "node:path";
 export function clearCache() {
   const cacheDir = path.resolve(process.cwd(), "node_modules/.cache/ghats");
   if (fs.existsSync(cacheDir)) {
-    fs.rmSync(cacheDir, { recursive: true });
+    fs.rmSync(cacheDir, { recursive: true, force: true });
   }
 }
