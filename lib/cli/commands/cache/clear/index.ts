1b5843f689f223c37eb591befd00d98b55b4e539
Koki Sato <kou.pg.0131@gmail.com>
2025-04-05 15:25:31 +0900

    feat: Implement `cache clear` command (#66)
    * chore: Create commands/ directory
    
    * feat: Implement `cache clear` command
    
    * fix: Enable force

---
+++ lib/cli/commands/cache/clear/index.ts
@@ -0,0 +1,9 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+export function clearCache() {
+  const cacheDir = path.resolve(process.cwd(), "node_modules/.cache/ghats");
+  if (fs.existsSync(cacheDir)) {
+    fs.rmSync(cacheDir, { recursive: true, force: true });
+  }
+}
--- lib/cli/index.ts
+++ lib/cli/index.ts
@@ -1,6 +1,7 @@
 import { Command } from "commander";
-import { build } from "./build";
-import { install } from "./install";
+import { build } from "./commands/build";
+import { clearCache } from "./commands/cache/clear";
+import { install } from "./commands/install";
 
 const program = new Command();
 
@@ -8,9 +9,17 @@ program
   .command("install [actions...]")
   .description("install actions to use in workflows")
   .action(install);
+
 program
   .command("build [workflows...]")
   .description("build github actions workflows")
   .action(build);
+
+program
+  .command("cache")
+  .description("manage cache")
+  .command("clear")
+  .description("clear cache")
+  .action(clearCache);
 
 program.parse(process.argv);
